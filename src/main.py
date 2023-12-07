3bf5527a6ddf6fc1a65ced419173b7222188b212
alehpineda <ale.hpineda@gmail.com>
2023-12-06 22:36:56 -0600

    fix tests

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+"""main.py"""
+
 from typing import Iterable
 
-import os
 
-def sum_even_numbers(numbers):
-    return sum(
-        num for num in numbers
-        if num % 2 == 0
-    )
+def sum_even_numbers(numbers: Iterable[int]) -> int:
+    """Sum even numbers
+
+    Args:
+        numbers (Iterative[int]): And iterative of ints
+
+    Returns:
+        int: The sum of ints
+    """
+    return sum(num for num in numbers if num % 2 == 0)
