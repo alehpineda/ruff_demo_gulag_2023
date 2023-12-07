3b3c92069fef1fdb8e0bbf636b6dc24ef0b1c65e
alehpineda <ale.hpineda@gmail.com>
2023-12-06 22:39:03 -0600

    fix tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@
 from src.main import sum_even_numbers
 
 
-def test_sum_even_numbers():
+def test_sum_even_numbers() -> None:
     """Test sum even numbers"""
     # Test case 1: Basic case with even numbers
     assert sum_even_numbers([1, 2, 3, 4, 5, 6]) == 12
@@ -16,7 +16,7 @@ def test_sum_even_numbers():
     # Add more test cases as needed
 
 
-def test_sum_even_numbers_coverage():
+def test_sum_even_numbers_coverage() -> None:
     """Test sum even numbers coverage"""
     # Additional test case to check coverage
     assert sum_even_numbers([2, 4, 6, 8]) == 20
